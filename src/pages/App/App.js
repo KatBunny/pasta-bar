3dba9e7a2667ee5b3ec8487b48c02f4fe5a3c792
Mitchell <mitchellbanrey@gmail.com>
2023-02-16 15:20:30 -0500

    EditMenuPage

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,35 +1,38 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
-import AuthPage from "../AuthPage/AuthPage"
+import AuthPage from "../AuthPage/AuthPage";
 import NewOrderPage from "../NewOrderPage/NewOrderPage";
-// import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
-import './App.css';
-import { getUser } from "../../utilities/users-service"
-
+import PastOrdersPage from "../PastOrdersPage/PastOrdersPage";
+import "./App.css";
+import { getUser } from "../../utilities/users-service";
+import EditMenuPage from "../EditMenuPage/EditMenuPage";
 
 export default function App() {
-  const [user, setUser] = useState(getUser())
+  const [user, setUser] = useState(getUser());
   //const [user, setUser] = useState(null)
-
+  // let adminStuff = "";
+  // if (user.isAdmin) {
+  //   adminStuff = ;
+  // }
 
   return (
     <main className="App">
-     <h1>iPasta!</h1>
+      <h1>iPasta!</h1>
       {/* terinary for conditional rendering */}
-      { user ?
+      {user ? (
         <>
-          <NavBar user={user} setUser={setUser}/>
+          <NavBar user={user} setUser={setUser} />
           <Routes>
+            <Route path="/admin" element={<EditMenuPage />}/>
             <Route path="/" element="" />
-            <Route path="/orders/new" element={<NewOrderPage />}/>
-            {/* <Route path="/orders" element={<OrderHistoryPage />}/> */}
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<PastOrdersPage />} />
           </Routes>
         </>
-        :
-        <AuthPage setUser={setUser}/>
-      }
-
+      ) : (
+        <AuthPage setUser={setUser} />
+      )}
     </main>
   );
 }
--- src/pages/EditMenuPage/EditMenuPage.js
+++ src/pages/EditMenuPage/EditMenuPage.js
@@ -1,1 +1,6 @@
 //for admin user to add/remove/update menu items
+export default function EditMenuPage(){
+    return(
+        <h1>Edit Menu Page</h1>
+    )
+}
