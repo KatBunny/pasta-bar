06fb0c65a8545b5c11de85fbeb0e8d879e583bc9
katherinewonders <108916070+katherinesinkler@users.noreply.github.com>
2023-02-17 15:23:45 -0700

    merging seed.js to the main

--- seed.js
+++ seed.js
@@ -1,35 +1,12 @@
 require('dotenv').config()
 require('.config/database')
 
-
-//const 
-//const
-//^^category will be created.
+const Ingredients = require('./models/ingredient')
 
 
 ;(async function () {
-	await Category.deleteMany({})
-const ingredients = await Category.create([
-    {name: 'Chicken', price:2, emoji:'🍗'},
-    {name:'Seafood',price:2, emoji:'🐟'},
-    {name:'Meatball',price:2, emoji:''},
-    {name:'Vegetarian Option',price:2, emoji:'🥗'},
-    {name:'Broccoli',price:2, emoji:'🥦'},
-    {name:'Peppers',price:2, emoji:'🌶️'},
-    {name:'Spinach',price:2, emoji:''},
-    {name:'Tomato',price:2, emoji:'🍅'},
-    {name:'Mushroom',price:2, emoji:'🍄'},
-    {name:'Spaghetti',price:2, emoji:'🍝'},
-    {name:'Tortellini',price:2, emoji:''},
-    {name:'Penne',price:2, emoji:''},
-    {name:'Orzo',price:2, emoji:''},
-    {name:'Ravioli',price:2, emoji:''},
-
-
-  ])
-
-	await order.deleteMany({})
-	const order = await Item.create([
+    await Ingredient.deleteMany({})
+	const ingredients = await Ingredient.create([
         {name: 'Chicken', price:2, emoji:'🍗'},
         {name:'Seafood',price:2, emoji:'🐟'},
         {name:'Meatball',price:2, emoji:''},
@@ -47,10 +24,9 @@ const ingredients = await Category.create([
         
     ])
 
-})
 
-console.log(items)
+console.log(ingredients)
 
-process.exit
-
+process.exit()
+})()
  
