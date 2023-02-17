e227f56109985ed483cc85a21ec5022d510f610a
Mitchell <mitchellbanrey@gmail.com>
2023-02-17 10:45:38 -0500

    Navbar functionality

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,13 +10,18 @@ export default function NavBar({user, setUser}) {
 
     return(
         <nav>
+            <h1 className="title">iPasta!</h1>
             <Link to="/orders">Past Orders</Link>
-            &nbsp; | &nbsp;
+            &nbsp;
             <Link to="/orders/new">New Order</Link>
-            &nbsp; | &nbsp;
-            <span>Welcome, {user.name}!</span>
-            &nbsp; | &nbsp;
+            &nbsp;
+            {user.isAdmin && <Link to="/admin">Edit Menu &nbsp;</Link> } 
+            {/* <span>Welcome, {user.name}!</span>
+            &nbsp;  &nbsp; */}
             <Link to="" onClick={handleLogOut}>Log Out</Link>
+            &nbsp;
+            
+            <span className="icon">{user.name.charAt(0).toUpperCase()}</span>
         </nav>
     )
 }
