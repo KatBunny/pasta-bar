77862a889d9fd2893f1a062ed6ba00d604b16dd4
Hanna <hanna.kim.x@gmail.com>
2023-02-18 17:03:58 -0600

    move showOrder and setShowOrders state to App

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -14,6 +14,7 @@ export default function App() {
   const [allIngredients, setAllIngredients] = useState([])
   const [availableIngredients, setAvailableIngredients] = useState([])
   const [newOrder, setNewOrder] = useState([])
+  const [showOrders, setShowOrders] = useState([])
 
   useEffect(function() {
     async function getAllIngredients() {
@@ -51,7 +52,14 @@ export default function App() {
                 setUser={setUser}
               />
             } />
-            <Route path="/orders" element={<PastOrdersPage user={user} setUser={setUser}/>} />
+            <Route path="/orders" element={
+              <PastOrdersPage 
+                user={user}
+                setUser={setUser}
+                showOrders={showOrders}
+                setShowOrders={setShowOrders}
+              />
+            } />
             <Route path="/*" element={<Navigate to="/orders/new" />} />
 
           </Routes>
