8002a1442f6aa10b2282c20852352325d538d8c7
katherinewonders <108916070+katherinesinkler@users.noreply.github.com>
2023-02-19 08:49:45 -0700

    Fixing my terminal for now

--- seed.js
+++ seed.js
@@ -7,20 +7,20 @@ const Ingredients = require('./models/ingredient')
 ;(async function () {
     await Ingredient.deleteMany({})
 	const ingredients = await Ingredient.create([
-        {name: 'Chicken', price:2, emoji:'🍗', isAvailable:true},
-        {name:'Seafood',price:2, emoji:'🐟', isAvailable:true},
-        {name:'Meatball',price:2, emoji:'🧆', isAvailable:true},
-        {name:'Vegetarian Option',price:2, emoji:'🥗', isAvailable:true},
-        {name:'Broccoli',price:2, emoji:'🥦', isAvailable:true},
-        {name:'Peppers',price:2, emoji:'🌶️', isAvailable:true},
-        {name:'Spinach',price:2, emoji:'🥬', isAvailable:true},
-        {name:'Tomato',price:2, emoji:'🍅', isAvailable:true},
-        {name:'Mushroom',price:2, emoji:'🍄', isAvailable:true},
-        {name:'Spaghetti',price:2, emoji:'🍝', isAvailable:true},
-        {name:'Tortellini',price:2, emoji:'image.png', isAvailable:true},
-        {name:'Penne',price:2, emoji:'image.png', isAvailable:true},
-        {name:'Orzo',price:2, emoji:'image.png', isAvailable:true},
-        {name:'Ravioli',price:2, emoji:'image.png', isAvailable:true},
+        {name: 'Chicken', price:'$9.99', emoji:'🍗', isAvailable:true},
+        {name:'Seafood',price:'$12.99', emoji:'🐟', isAvailable:true},
+        {name:'Meatball',price:'$8.99', emoji:'🧆', isAvailable:true},
+        {name:'Eggplant Parmigiano',price:'$8.99', emoji:'🍆', isAvailable:true},
+        {name:'Broccoli',price:'$2.99', emoji:'🥦', isAvailable:true},
+        {name:'Peppers',price:'$2.99', emoji:'🌶️', isAvailable:true},
+        {name:'Spinach',price:'$2.99', emoji:'🥬', isAvailable:true},
+        {name:'Tomato',price:'$2.99', emoji:'🍅', isAvailable:true},
+        {name:'Mushroom',price:'$2.99', emoji:'🍄', isAvailable:true},
+        {name:'Spaghetti',price:'$6.99', emoji:'🍝', isAvailable:true},
+        {name:'Tortellini',price:'$6.99', emoji:'image.png', isAvailable:true},
+        {name:'Penne',price:'$6.99', emoji:'image.png', isAvailable:true},
+        {name:'Orzo',price:'$6.99', emoji:'image.png', isAvailable:true},
+        {name:'Ravioli',price:'$6.99', emoji:'image.png', isAvailable:true},
         
     ])
 
