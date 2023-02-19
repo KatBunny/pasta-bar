fac1427252e09ea1f8441c2dba755c96f6560896
katherinewonders <108916070+katherinesinkler@users.noreply.github.com>
2023-02-19 01:31:35 -0700

    Added some images of pasta

--- seed.js
+++ seed.js
@@ -9,18 +9,18 @@ const Ingredients = require('./models/ingredient')
 	const ingredients = await Ingredient.create([
         {name: 'Chicken', price:2, emoji:'🍗', isAvailable:true},
         {name:'Seafood',price:2, emoji:'🐟', isAvailable:true},
-        {name:'Meatball',price:2, emoji:'', isAvailable:true},
+        {name:'Meatball',price:2, emoji:'🧆', isAvailable:true},
         {name:'Vegetarian Option',price:2, emoji:'🥗', isAvailable:true},
         {name:'Broccoli',price:2, emoji:'🥦', isAvailable:true},
         {name:'Peppers',price:2, emoji:'🌶️', isAvailable:true},
-        {name:'Spinach',price:2, emoji:'', isAvailable:true},
+        {name:'Spinach',price:2, emoji:'🥬', isAvailable:true},
         {name:'Tomato',price:2, emoji:'🍅', isAvailable:true},
         {name:'Mushroom',price:2, emoji:'🍄', isAvailable:true},
         {name:'Spaghetti',price:2, emoji:'🍝', isAvailable:true},
-        {name:'Tortellini',price:2, emoji:'', isAvailable:true},
-        {name:'Penne',price:2, emoji:'', isAvailable:true},
-        {name:'Orzo',price:2, emoji:'', isAvailable:true},
-        {name:'Ravioli',price:2, emoji:'', isAvailable:true},
+        {name:'Tortellini',price:2, emoji:'image.png', isAvailable:true},
+        {name:'Penne',price:2, emoji:'image.png', isAvailable:true},
+        {name:'Orzo',price:2, emoji:'image.png', isAvailable:true},
+        {name:'Ravioli',price:2, emoji:'image.png', isAvailable:true},
         
     ])
 
