a1437a0f1315fa67c35d02072ae10d6efa1f402b
Hanna <hanna.kim.x@gmail.com>
2023-02-19 15:13:00 -0600

    display order createdAt date in PastOrders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,9 +2,12 @@
 
 export default function Order({order}){
     const ingredientItems = order.ingredients.map((ingredient, index) => (<p key={index}>{ingredient.name}</p>))
+
+    // need a virtual method to convert createdAt date to Month Date, Year
+
     return(
         <>
-            <p>Order: {order._id}</p>
+            <p>Order: {order.createdAt}</p>
             <p>Total Price: ${order.orderTotal.toFixed(2)}</p>
             <p>Ingredients:</p>
             {ingredientItems}
