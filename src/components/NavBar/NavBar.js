9f1bcb015ead32649e0fa094a7057e3404a926b2
Mitchell <mitchellbanrey@gmail.com>
2023-02-19 15:52:50 -0500

    nav bar css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import * as userService from "../../utilities/users-service"
+import "./NavBar.css"
  
 export default function NavBar({user, setUser, order, resetOrder}) {
 
@@ -10,19 +11,22 @@ export default function NavBar({user, setUser, order, resetOrder}) {
     }
 
     return(
-        <nav>
+        <nav className="NavBar">
+            <div className="design">
             <h1 className="title">iPasta!</h1>
-            <Link to="/orders">Past Orders</Link>
+            
+            <Link to="/orders" className="links">Past Orders</Link>
             &nbsp;
-            <Link to="/orders/new">New Order</Link>
+            <Link to="/orders/new" className="links">New Order</Link>
             &nbsp;
-            {user.isAdmin && <Link to="/edit-menu">Edit Menu &nbsp;</Link> } 
+            {user.isAdmin && <Link to="/edit-menu" className="links">Edit Menu &nbsp;</Link> } 
             {/* <span>Welcome, {user.name}!</span>
             &nbsp;  &nbsp; */}
-            <Link to="" onClick={handleLogOut}>Log Out</Link>
+            <Link to="" onClick={handleLogOut} className="links">Log Out</Link>
             &nbsp;
             
             <span className="icon">{user.name.charAt(0).toUpperCase()}</span>
+            </div>
         </nav>
     )
 }
