f15003416e29b3d1980b8cb616eef32bf5e3cb23
Hanna <hanna.kim.x@gmail.com>
2023-02-20 16:40:12 -0600

    adjust login/signup form alignment, edit authform styles

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
 import LoginForm from "../../components/LoginForm/LoginForm"
+import "./AuthPage.css"
 
-export default function AuthPage({ setUser, order, resetOrder }) {
+export default function AuthPage({ setUser }) {
     
     const [showSignUp, setShowSignUp] = useState(false)
     
@@ -13,9 +14,16 @@ export default function AuthPage({ setUser, order, resetOrder }) {
             ) : (
                 <LoginForm setUser={setUser} />
             )}
-            <a onClick={()=> setShowSignUp(!showSignUp)}>
-                {showSignUp ? 'Already have an account? Login' : 'First time ordering? Sign up here'}
-            </a>
+
+                {showSignUp ? 
+                    <>
+                        <p>Already have an account? <a className="authpage-link" onClick={()=> setShowSignUp(!showSignUp)}>Login here</a></p>
+                    </>
+                : 
+                <>
+                    <p>First time ordering? Sign up <a className="authpage-link" onClick={()=> setShowSignUp(!showSignUp)}>here</a></p>
+                </>}
+
         </>
     )
 }
