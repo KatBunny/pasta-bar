94561aa78a78d5e8848ba8a2cf360ef429b0c1da
katherinewonders <108916070+katherinesinkler@users.noreply.github.com>
2023-02-21 17:58:55 -0700

    Commiting and pulling for the night. no need to do more besides make Nav a little more petit in porportion to the page.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,17 +14,17 @@ export default function NavBar({user, setUser, order, resetOrder}) {
         <nav className="NavBar">
             <div className="design">
             <h1 className="title">iPasta!</h1>
-            
-            <Link to="/orders" className="links">Past Orders</Link>
+             <h2><span>Welcome, {user.name}!</span></h2>
+            &nbsp;  &nbsp;
+            <h3><Link to="/orders" className="links">Past Orders</Link></h3>
             &nbsp;
-            <Link to="/orders/new" className="links">New Order</Link>
+            <h3><Link to="/orders/new" className="links">New Order</Link></h3>
             &nbsp;
             {user.isAdmin && <Link to="/edit-menu" className="links">Edit Menu &nbsp;</Link> } 
             {/* <span>Welcome, {user.name}!</span>
             &nbsp;  &nbsp; */}
-            <Link to="" onClick={handleLogOut} className="links">Log Out</Link>
+            <h3><Link to="" onClick={handleLogOut} className="links">Log Out</Link></h3>
             &nbsp;
-            
             <span className="icon">{user.name.charAt(0).toUpperCase()}</span>
             </div>
         </nav>
