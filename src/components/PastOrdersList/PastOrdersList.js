a3d58b3d7347e04c1653c43d78286dfdc2ac777d
Mitchell <mitchellbanrey@gmail.com>
2023-02-22 00:14:53 -0500

    fixes

--- src/components/PastOrdersList/PastOrdersList.js
+++ src/components/PastOrdersList/PastOrdersList.js
@@ -1,11 +1,11 @@
 // shows all (past) orders into PastOrdersPage
 import Order from "../Order/Order"
 
-export default function PastOrdersList({ user, setUser, myOrders }){
+export default function PastOrdersList({ user, setUser, myOrders, ingredients }){
     // map myOrders
     console.log(myOrders)
 
-    const myOrderItems = myOrders.map((myOrder, index) => (<Order order={myOrder} key={index}/>))
+    const myOrderItems = myOrders.map((myOrder, index) => (<Order order={myOrder} key={index} />))
 
     return(
         <div className="past-orders-container">
